b8f909f727138e646d1f217153ffea9e83ff1f74
anup-a <aglawe.anup22@gmail.com>
2020-11-16 20:47:24 +0530

    fixed double post request bug

--- server/http_request.py
+++ server/http_request.py
@@ -9,9 +9,47 @@ def handle_GET(res_sock, req_line):
     http_req, req_uri, protocol_version = req_line
 
     req_uri = req_uri[1:]
+    handle_response(res_sock, req_uri)
+
+
+def handle_POST(res_sock, req_line, req_headers):
+
+    http_req, req_uri, protocol_version = req_line
+
+    content_length = int(req_headers['Content-Length'])
+    body = b''
+
+    for i in range(content_length):
+        msg = res_sock.recv(1)
+        body += msg
+
+    update_to_db(body)
+    handle_redirect(res_sock, '')
+
+
+def body_parser(body):
+    body = body.decode("utf-8")
+    data = body.split("=")
+    return {data[0]: data[1]}
+
+
+def update_to_db(body):
+    parsedText = body_parser(body)
+    database["feed"].append(parsedText["name"])
+
+    with open('server/db/data.json', 'w') as f:
+        json.dump(database, f)
+
+
+def handle_redirect(res_sock, req_uri):
+    handle_response(res_sock, req_uri)
+
+
+def handle_response(res_sock, req_uri):
     if req_uri == '':
         req_uri = 'index.html'
 
+    print(req_uri)
     file = join('server/src', req_uri)
 
     file_size = get_size(file)
@@ -28,32 +66,3 @@ def handle_GET(res_sock, req_line):
     http_res += http_body
 
     res_sock.sendall(http_res)
-
-
-def handle_POST(res_sock, req_line, req_headers):
-
-    http_req, req_uri, protocol_version = req_line
-    print(http_req, req_uri, protocol_version)
-
-    content_length = int(req_headers['Content-Length'])
-    body = b''
-
-    for i in range(content_length):
-        msg = res_sock.recv(1)
-        body += msg
-
-    update_to_db(body)
-
-
-def body_parser(body):
-    body = body.decode("utf-8")
-    data = body.split("=")
-    return {data[0]: data[1]}
-
-
-def update_to_db(body):
-    parsedText = body_parser(body)
-    database["feed"].append(parsedText["name"])
-
-    with open('server/db/data.json', 'w') as f:
-        json.dump(database, f)
