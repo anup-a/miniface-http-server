625bdfe0cddc92fbea2bae2c64071ec99558b804
Aditya Garg <aditya.garg@iitgn.ac.in>
2020-11-19 00:10:11 +0530

    Login_page

--- server/controller.py
+++ server/controller.py
@@ -1,5 +1,7 @@
 import sqlite3
 from body_parser import body_parser
+from os.path import join
+from helpers import *
 
 
 def add_post(post_id, post_body, user_id):
@@ -52,21 +54,113 @@ def handleDBFetchAPI(req_uri):
         return get_posts()
 
 
-def handleDBPushAPI(req_uri, body):
+def login(user_name, password):
+    con = sqlite3.connect('server/db/accounts.db')
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("select * from accounts where user_name=?", (user_name,))
+
+    c = cur.fetchone()
+    # posts = []
+    if c:
+        if c[2]==password:
+            return((1, c[0]))
+        else:
+            return((0, 'The password was wrong.'))
+    else:
+        return((0, 'The username does not exist.'))
+    # return posts
+
+
+def handleDBPushAPI(res_sock, req_uri, body):
     if req_uri == '/addpost.html' or req_uri == 'addpost.html':
         print("Adding to SQLite Database.....")
         add_post('2', body["name"], '1')
         print("Done. Added")
+        return req_uri
+        
+    if req_uri=='/login_page.html' or req_uri == 'login_page.html':
+        print(body)
+        res = login(body['username'], body['password'])
+        if res[0]:
+            print("authenticated.")
+            handle_redirect(res_sock, user_id=res[1])
+        else:
+            print(res[1])
+            handle_redirect(res_sock)
 
 
-def addtoDB(req_uri, body):
+def addtoDB(res_sock, req_uri, body):
     print(req_uri, body)
     parsedText = body_parser(body)
-    handleDBPushAPI(req_uri, parsedText)
+    handleDBPushAPI(res_sock, req_uri, parsedText)
 
 
 def generateHTML(template, loader, req_uri):
     data = handleDBFetchAPI(req_uri)
     file_data = template.render({'data': data}, loader=loader).encode('utf-8')
+
+    return file_data
+
+def handle_redirect(res_sock, req_uri='/index.html', user_id=None):
+    redir_param = {
+        "redirect": True,
+        "path": req_uri,
+        "user_id": user_id,
+    }
+    handle_response(res_sock, req_uri, redir_param)
+
+
+def handle_response(res_sock, req_uri, redir_param={}):
+
+    if req_uri == '':
+        req_uri = 'index.html'
+
+    file = join('server/src', req_uri)
+
+    file_size = get_size(file)
+    http_res = gen_status(file_size)
+
+    http_body = b'\r\n'
+
+    http_body += read_file(file, req_uri, redir_param)
+
+    res_headers = get_response_headers(file)
+
+    for header in res_headers:
+        http_res += header
+
+    http_res += http_body
+
+    res_sock.sendall(http_res)
+
+def read_file(file, req_uri, redir_param):
+
+    redirect = False
+    path = "/"
+
+    if 'redirect' in redir_param.keys():
+        redirect = redir_param['redirect']
+        path = redir_param['path']
+
+    file_data = b''
+
+    if get_size(file):
+
+        if not redirect:
+            template = loader.load_template(file)
+            file_data = generateHTML(template, loader, req_uri)
+
+        else:
+            res = open('server/src/redirect.html', 'r+b')
+            for i in range(get_size(file)):
+                file_data += res.read()
+
+        """
+        For Static files
+        """
+        # res = open(file, 'r+b')
+        # for i in range(get_size(file)):
+        #     file_data += res.read()
 
     return file_data
--- server/http_request.py
+++ server/http_request.py
@@ -1,7 +1,5 @@
-from os.path import join
-from helpers import *
 from body_parser import body_parser
-import controller
+from controller import *
 
 
 def handle_GET(res_sock, req_line):
@@ -24,39 +22,9 @@ def handle_POST(res_sock, req_line, req_headers):
     # json
     # update_to_db(body)
 
-    controller.addtoDB(req_uri, body)
+    addtoDB(res_sock, req_uri, body)
 
     # Redirect to / or /index.html
-    handle_redirect(res_sock, '')
+    # handle_redirect(res_sock, '')
 
 
-def handle_redirect(res_sock, req_uri):
-    redir_param = {
-        "redirect": True,
-        "path": '/index.html',
-    }
-    handle_response(res_sock, req_uri, redir_param)
-
-
-def handle_response(res_sock, req_uri, redir_param={}):
-
-    if req_uri == '':
-        req_uri = 'index.html'
-
-    file = join('server/src', req_uri)
-
-    file_size = get_size(file)
-    http_res = gen_status(file_size)
-
-    http_body = b'\r\n'
-
-    http_body += read_file(file, req_uri, redir_param)
-
-    res_headers = get_response_headers(file)
-
-    for header in res_headers:
-        http_res += header
-
-    http_res += http_body
-
-    res_sock.sendall(http_res)
